984ce382c80f76e377b9cbdbfb4274eab1885d37
GuilleSuarezC <uo278552@uniovi.es>
2025-04-15 16:09:28 +0200

    LTA fully implemented in ConsultEventStatus

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -44,11 +44,8 @@ INSERT INTO "Balance" (concept, event_id, amount, balance_status, description) V
     ('Sponsorship Innovate Asturias', 2, 4000.0, 'Paid', 'Innovate Asturias'),
     ('Expense', 3, -1000.0, 'Estimated', 'Rent a building for the event'),
     ('AI Research Fund', 3, 5000.0, 'Estimated', 'AI Research Fund sponsorship'),
-    ('Sponsorship Future Innovators', 3, 5000.0, 'Estimated', 'Future Innovators sponsorship'),
-    ('LTA - Tech Solutions S.L.', 2, 1000.0, 'Estimated', 'LTA Hackathon UNIOVI IV Tech Solutions S.L.'),
-    ('LTA - Tech Solutions S.L.', 3, 1000.0, 'Estimated', 'LTA Asturias AI Summit III Tech Solutions S.L.'),
-    ('LTA - Innovate Asturias', 1, 1000.0, 'Estimated', 'LTA ImpulsoTIC Week XII Innovate Asturias'),
-    ('LTA - Innovate Asturias', 3, 1000.0, 'Estimated', 'LTA Asturias AI Summit III Innovate Asturias');
+    ('Sponsorship Future Innovators', 3, 5000.0, 'Estimated', 'Future Innovators sponsorship');
+
     
 -- Insertar datos en la tabla "Sponsorship"
 INSERT INTO "Sponsorship" (sponsorship_name, sponsorship_agreementDate, company_id, event_id, gb_id, balance_id) VALUES
@@ -82,15 +79,5 @@ INSERT INTO "SponsorshipLevel" (level_name, level_price, event_id) VALUES
     ('Diamond', 5000.0, 3),
     ('Gold', 3000.0, 3),
     ('Contributor', 1500.0, 3);
-    
-INSERT INTO "LongTermAgreement" (lta_startDate, lta_endDate, lta_totalFee, company_id) VALUES
-	("2025-06-11", "2027-06-11", 10000.0, 1),
-	("2024-05-10", "2026-05-10", 8000.0, 2);
-
-INSERT INTO "LTA_Event" (lta_id, event_id, balance_id) VALUES
-	(1, 2, 6),
-	(1, 3, 7),
-	(2, 1, 8),
-	(2, 3, 9);
 
 COMMIT;
